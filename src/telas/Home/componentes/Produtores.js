a9b5c71229c17d47aec9e87f35d5c029fd0b0514
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2022-04-16 13:06:05 -0300

    03.03 - Armazenando produtores

--- src/telas/Home/componentes/Produtores.js
+++ src/telas/Home/componentes/Produtores.js
@@ -1,18 +1,48 @@
-import React, {useEffect} from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {FlatList, StyleSheet, Text} from 'react-native';
 import {carregaProdutores} from '../../../servicos/carregaDados';
 
-export default function Produtores() {
+export default function Produtores({topo: Topo}) {
+  const [titulo, setTitulo] = useState('');
+  const [lista, setLista] = useState([]);
+
   useEffect(() => {
     return () => {
       const retorno = carregaProdutores();
-      console.log(retorno);
+      setTitulo(retorno.titulo);
+      setLista(retorno.lista);
     };
   }, []);
 
+  const TopoLista = () => {
+    return (
+      <>
+        <Topo />
+        <Text style={styleProdutores.titulo}>{titulo}</Text>
+      </>
+    );
+  };
+  const ItemLista = ({item: {nome}}) => {
+    return <Text>{nome}</Text>;
+  };
+
   return (
-    <View>
-      <Text>Produtores</Text>
-    </View>
+    <FlatList
+      data={lista}
+      renderItem={ItemLista}
+      ListHeaderComponent={TopoLista}
+      keyExtractor={({nome}) => nome}
+    />
   );
 }
+
+const styleProdutores = StyleSheet.create({
+  titulo: {
+    fontSize: 20,
+    lineHeight: 36,
+    marginHorizontal: 16,
+    marginTop: 16,
+    fontWeight: 'bold',
+    color: '#464646',
+  },
+});
--- src/telas/Home/index.js
+++ src/telas/Home/index.js
@@ -3,10 +3,5 @@ import Topo from './componentes/Topo';
 import Produtores from './componentes/Produtores';
 
 export default function Home() {
-  return (
-    <>
-      <Topo />
-      <Produtores />
-    </>
-  );
+  return <Produtores topo={Topo} />;
 }
