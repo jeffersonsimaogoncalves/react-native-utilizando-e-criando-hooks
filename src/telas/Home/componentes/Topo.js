ba9691a35a42b1bf1bd5c2d9d9820e2ceb1a19f3
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2022-04-16 12:24:08 -0300

    02.07 - Consumindo o serviço

--- src/telas/Home/componentes/Topo.js
+++ src/telas/Home/componentes/Topo.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
 import logo from '../../../assets/logo.png';
+import {carregaTopo} from '../../../servicos/carregaDados';
 
-export default function Topo() {
-  return (
-    <View style={styleTopo.topo}>
-      <Image source={logo} style={styleTopo.image} />
-      <Text style={styleTopo.boasVindas}>Olá Jefferson</Text>
-      <Text style={styleTopo.legenda}>Encontre os melhores produtores</Text>
-    </View>
-  );
+export default class Topo extends React.Component {
+  atualizaTopo() {
+    const retorno = carregaTopo();
+    console.log(retorno);
+  }
+
+  componentDidMount() {
+    this.atualizaTopo();
+  }
+
+  render() {
+    return (
+      <View style={styleTopo.topo}>
+        <Image source={logo} style={styleTopo.image} />
+        <Text style={styleTopo.boasVindas}>Olá Jefferson</Text>
+        <Text style={styleTopo.legenda}>Encontre os melhores produtores</Text>
+      </View>
+    );
+  }
 }
 
 const styleTopo = StyleSheet.create({
